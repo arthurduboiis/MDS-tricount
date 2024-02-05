fcafffceba371016a567f740d5065634b05c8f91
Arthur DUBOIS <arthur.dbs70@gmail.com>
2024-02-05 15:00:12 +0100

    feat: navbar design

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const NavBarComponent = () => {
+
+    const goToSettings = () => {
+        console.log('go to settings');
+    }
+
     return (
-        <div>
-        <nav>
-            <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            </ul>
-        </nav>
-        </div>
+            <nav className="bg-slate-800 flex justify-between items-center relative top-0 w-full h-16 p-4">
+                <img className='' src='/AppImages/android/android-launchericon-48-48.png'/>
+                <img className="h-16" src='/tricount_name.png'/>
+                <div className="flex flex-col space-y-1 pr-2" onClick={goToSettings}>
+                    <div className='h-2 w-2 border rounded-full bg-white'></div>
+                    <div className='h-2 w-2 border rounded-full bg-white'></div>
+                    <div className='h-2 w-2 border rounded-full bg-white'></div>
+
+                </div>
+                
+                
+                
+            </nav>
     );
     }
 
