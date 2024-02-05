b17db4ba079b88fa7e1e4a16c53068d5edecd6c1
Arthur DUBOIS <arthur.dbs70@gmail.com>
2024-02-05 14:20:04 +0100

    feat: add tailwindcss

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.mclqegul188"
+    "revision": "0.h5maq223qsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
