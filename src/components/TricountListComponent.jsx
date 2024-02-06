ceaa14516bf3832bae0ecb1b6b488365cd230dcd
baptiste <baptiste.barcon@edu.univ-fcomte.fr>
2024-02-06 12:22:27 +0100

    feat: create list of tricount

---
+++ src/components/TricountListComponent.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const TricountListComponent = (tricount) => {
+  const detail = () => {
+    console.log("detail");
+  };
+
+  return (
+    <div className="flex justify-between items-center text-2xl font-sans">
+      <div className="flex flex-col items-start justify-between pt-2 pl-4">
+        <strong>{tricount.title}</strong>
+        <div className="text-xl italic pb-4">{tricount.description}</div>
+      </div>
+      <div className="flex">
+          {tricount.notifications > 0 && (
+            <div>
+              <div className="w-9 h-9 border rounded-full bg-blue-400">
+                {tricount.notifications}
+              </div>
+            </div>
+          )}
+        <div onClick={detail} className="size-10 cursor-pointer">
+          <img src="/flecheDroite.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TricountListComponent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
-import NavBarComponent from '../components/NavBarComponent';
-import { useNavigate } from 'react-router';
+import React from "react";
+import NavBarComponent from "../components/NavBarComponent";
+import TricountListComponent from "../components/tricountListComponent";
+import { useNavigate } from "react-router";
 
 const Home = () => {
-    const navigate = useNavigate();
+  const tricounts = [
+    {
+      title: "Lyon",
+      description: "ça coûte chère zebi",
+      notifications: 2,
+    },
+    {
+      title: "Voyage",
+      description: "les vacances, c'est bien",
+      notifications: 2,
+    },
+    {
+      title: "Autre exemple",
+      description: "la description de l'exemple",
+      notifications: 0,
+    },
+  ];
+  const navigate = useNavigate();
 
-    const goToNewTricount = () => {
-        navigate('/new-tricount');
-    }
-    return (
-        <div className='w-full'>
-        <NavBarComponent />
-        <h2 className='text-3xl font-bold underline'>Tricount</h2>
-        <div className='flex justify-center items-center absolute bottom-2 right-2 h-14 w-14 cursor-pointer' onClick={goToNewTricount}>
-            <img src="/add.png" alt="add button"/>
-        </div>
+  const goToNewTricount = () => {
+    navigate("/new-tricount");
+  };
+  return (
+    <div className="w-full">
+      <NavBarComponent />
+      <div className="divide-y-2 divide-blue-300">
+        {tricounts.map((tricount, index) => (
+          <TricountListComponent
+            key={index}
+            title={tricount.title}
+            description={tricount.description}
+            notifications={tricount.notifications}
+          />
+        ))}
       </div>
-    );
-}
+      <div
+        className="flex justify-center items-center absolute bottom-2 right-2 h-14 w-14 cursor-pointer"
+        onClick={goToNewTricount}
+      >
+        <img src="/add.png" alt="add button" />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
