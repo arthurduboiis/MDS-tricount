57cff51c2e82d5418bd3df0023b53e2f3c9ba2cb
Arthur DUBOIS <arthur.dbs70@gmail.com>
2024-02-06 13:51:50 +0100

    feat: tricount page front

---
+++ src/pages/Tricount.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBarTicountComponent from "../components/NavBarTicountComponent";
+import ExpenseBalanceTabComponent from "../components/ExpenseBalanceTabComponent";
+import TricountTotalFooterComponent from "../components/TricountTotalFooterComponent";
+
+const Tricount = () => {
+  const tricount = {
+    name: "Lyon",
+    users: ["arthur", "baptiste"],
+  };
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-between">
+      <div className="">
+        {" "}
+        <NavBarTicountComponent name={tricount.name} users={tricount.users} />
+        <ExpenseBalanceTabComponent />
+      </div>
+      <TricountTotalFooterComponent />
+    </div>
+  );
+};
+
+export default Tricount;
